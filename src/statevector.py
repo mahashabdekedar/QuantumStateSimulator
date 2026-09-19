e76ed81732b8a1c3ca32ac1638333f8b01f23c86
Kedar Mahashabde <kedar.cp3@gmail.com>
2026-09-19 03:28:03 -0400

    Added numpy statevector with basic functionality

--- src/statevector.py
+++ src/statevector.py
@@ -4,3 +4,13 @@ import numpy as np
 class statevector:
 
     def __init__(self, numQubits:int):
+        if numQubits > 5:
+            raise ValueError('Too many qubits!')
+        self.state = np.ndarray(2**numQubits, dtype=np.cdouble).reshape(np.ones(numQubits)*2)
+
+    def isvalid(self) -> bool:
+        return np.isclose(np.linalg.norm(self.state), 1)
+
+    def ispure(self) -> bool:
+        return np.isclose(np.linalg.trace(np.linalg.matrix_power(self.state.T @ self.state, 2)), 1)
+
